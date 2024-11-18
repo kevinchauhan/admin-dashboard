8898d2f42a780dc46e2dab1a0db4d1a917dffb9b
kevinchauhan <rwnkevinchauhan@gmail.com>
2024-11-18 14:19:48 +0530

    add user table

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,10 +1,41 @@
 import { RightOutlined } from "@ant-design/icons"
-import { Breadcrumb } from "antd"
+import { useQuery } from "@tanstack/react-query"
+import { Breadcrumb, Table } from "antd"
 import { NavLink } from "react-router-dom"
+import { getUsers } from "../../http/api"
+import { User } from "../../types"
+
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'firstName',
+        key: 'firstName',
+        render: (_text: string, record: User) => (<div>
+            {record.firstName} {record.lastName}
+        </div>),
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+    },
+    {
+        title: 'Role',
+        dataIndex: 'role',
+        key: 'role',
+    },
+]
 
 const Users = () => {
+
+    const { data: users, isLoading, isError, error } = useQuery({
+        queryKey: ['users'],
+        queryFn: () => getUsers().then(res => res.data.data),
+        retry: 1
+    })
+
     return (
-        <div>
+        <>
             <Breadcrumb separator={<RightOutlined />} items={[
                 {
                     title: <NavLink to='/'>Dashboard</NavLink>,
@@ -13,7 +44,10 @@ const Users = () => {
                     title: 'User',
                 },
             ]} />
-        </div>
+            {isLoading && <div>Loading...</div>}
+            {isError && <div>{error.message}</div >}
+            <Table style={{ marginTop: '15px' }} columns={columns} dataSource={users} />
+        </>
     )
 }
 
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,13 @@ export type Credentials = {
     email: string
     password: string
 }
+
+export type User = {
+    id: number
+    firstName: string
+    lastName: string
+    email: string
+    role: string
+    createdAt: string
+    updatedAt: string
+}
