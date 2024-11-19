388dc84a7260dc1d34241a036182638f77c7e4fe
kevinchauhan <rwnkevinchauhan@gmail.com>
2024-11-19 14:52:06 +0530

    add user form validation

--- src/pages/users/forms/UserForm.tsx
+++ src/pages/users/forms/UserForm.tsx
@@ -17,22 +17,22 @@ const UserForm = () => {
                 <Card title='Basic Information' bordered={false}>
                     <Row gutter={12}>
                         <Col span={12}>
-                            <Form.Item label="First Name" name='firstName'>
+                            <Form.Item label="First Name" name='firstName' rules={[{ required: true, message: 'First Name is required' }]}>
                                 <Input placeholder="" />
                             </Form.Item>
                         </Col>
                         <Col span={12}>
-                            <Form.Item label="Last Name" name='lastName'>
+                            <Form.Item label="Last Name" name='lastName' rules={[{ required: true, message: 'Last Name is required' }]}>
                                 <Input placeholder="" />
                             </Form.Item>
                         </Col>
                         <Col span={12}>
-                            <Form.Item label="Email" name='email'>
+                            <Form.Item label="Email" name='email' rules={[{ required: true, message: 'Email is required' }, { type: 'email', message: 'Please enter valid email' }]}>
                                 <Input placeholder="" />
                             </Form.Item>
                         </Col>
                         <Col span={12}>
-                            <Form.Item label="Phone Number" name='phonenumber'>
+                            <Form.Item label="Phone Number" name='phonenumber' >
                                 <Input placeholder="" />
                             </Form.Item>
                         </Col>
@@ -41,7 +41,7 @@ const UserForm = () => {
                 <Card style={{ marginTop: '12px' }} title='Security Information' bordered={false}>
                     <Row gutter={12}>
                         <Col span={12}>
-                            <Form.Item label="Passwrod" name='password'>
+                            <Form.Item label="Passwrod" name='password' rules={[{ required: true, message: 'Password is required' }]}>
                                 <Input type="password" placeholder="***" />
                             </Form.Item>
                         </Col>
@@ -55,7 +55,7 @@ const UserForm = () => {
                 <Card style={{ marginTop: '12px' }} title='Roles' bordered={false}>
                     <Row gutter={12}>
                         <Col span={12}>
-                            <Form.Item label="Select Role" name='role'>
+                            <Form.Item label="Select Role" name='role' rules={[{ required: true, message: 'Please select role' }]}>
                                 <Select placeholder='Role' style={{ width: '100%' }} allowClear={true}>
                                     <Select.Option value="admin">Admin</Select.Option>
                                     <Select.Option value="manager">Manager</Select.Option>
@@ -64,7 +64,7 @@ const UserForm = () => {
                             </Form.Item>
                         </Col>
                         <Col span={12}>
-                            <Form.Item label="Select Restaurant" name='tenantId'>
+                            <Form.Item label="Select Restaurant" name='tenantId' rules={[{ required: true, message: 'Please select restaurant' }]} >
                                 <Select placeholder='Restaurant' style={{ width: '100%' }} allowClear={true}>
                                     {tenants?.length > 0 &&
                                         tenants.map((tenant: Tenant) =>
